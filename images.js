e1e9741b7a2237a70ab423b132bad29b7c8e7aec
Mandeeq <imanjamal370@gmail.com>
2024-07-23 11:57:30 +0300

    fixed bugs

--- images.js
+++ images.js
@@ -5,21 +5,33 @@ document.addEventListener('DOMContentLoaded', () => {
     const rightButton = document.querySelector('.slide.right');
     
     let currentIndex = 0;
+    const imageWidth = images[0].clientWidth; // Get the width of one image
+    const intervalTime = 5000; // 5 seconds
 
     function updateSlider() {
-        const imageWidth = images[0].clientWidth; // Get the width of one image
         imagesContainer.style.transform = `translateX(${-currentIndex * imageWidth}px)`;
     }
 
-    leftButton.addEventListener('click', () => {
+    function nextSlide() {
+        currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
+        updateSlider();
+    }
+
+    function previousSlide() {
         currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
         updateSlider();
+    }
+
+    leftButton.addEventListener('click', () => {
+        previousSlide();
     });
 
     rightButton.addEventListener('click', () => {
-        currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
-        updateSlider();
+        nextSlide();
     });
+
+    // Auto-slide function
+    setInterval(nextSlide, intervalTime);
 
     // Initial call to set the correct position
     updateSlider();
